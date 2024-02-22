6cad43345b0ca8c2ee21e0f91d4e1f8bca0f0aa1
Merlin-Luca <merlinlucapro@gmail.com>
2024-02-22 17:41:13 +0100

    maj 1.0

--- ressources/script.js
+++ ressources/script.js
@@ -27,3 +27,85 @@ let suggestions_cible = document.getElementById('suggestions')
 suggestions.addEventListener('click', ()=>{
     window.scrollTo(0, suggestions_cible.offsetTop - 100);
 })
+
+let loaderScreen = document.querySelector(".loader")
+let page = document.querySelector(".page")
+
+function load(){
+    setTimeout(()=>{
+    loaderScreen.style.display = "none";
+    }, 1450)
+}
+load()
+
+// 
+
+const slides = document.querySelectorAll(".slide"); 
+let currentSlide = 0; 
+let totalSlides = slides.length;
+
+function showSlide_right(index) {
+    slides.forEach((slide) => { 
+        slide.classList.remove("active"); 
+        slide.classList.remove("right"); 
+        slide.classList.remove("left"); 
+    }); 
+    slides[index].classList.add("active");
+    slides[index].classList.add("right");
+    slides[index].style.transform = "translateX(0%)"
+    if(currentSlide == slides.length - 1)
+    {
+        slides
+    }
+}
+function showSlide_left(index) {
+    slides.forEach((slide) => {
+        slide.classList.remove("active"); 
+        slide.classList.remove("right"); 
+        slide.classList.remove("left"); 
+    }); 
+    slides[index].classList.add("active");
+    slides[index].classList.add("left");
+    slides[index].style.transform = "translateX(0%)"
+}
+function nextSlide() {
+    slides[currentSlide].style.transform = "translateX(-35%)"
+    currentSlide = (currentSlide + 1) % totalSlides; 
+    showSlide_right(currentSlide); }
+    
+    function previousSlide() { 
+        slides[currentSlide].style.transform = "translateX(35%)"
+        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide_left(currentSlide);
+}
+
+document.querySelector(".prev").addEventListener("click", function(){ previousSlide(); });
+
+document.querySelector(".next").addEventListener("click", function(){ nextSlide(); });
+
+showSlide_left(currentSlide);
+
+
+//-------------------------------------------------
+// const slides = document.querySelectorAll(".slide");
+// let currentSlide = 0;
+
+// function showSlide(index) {
+//   slides.forEach((slide) => {
+//     slide.classList.remove("active");
+//   });
+//   slides[index].classList.add("active");
+// }
+
+// document.querySelector(".prev").addEventListener("click", function() {
+//   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+//   showSlide(currentSlide);
+// });
+
+// document.querySelector(".next").addEventListener("click", function() {
+//   currentSlide = (currentSlide + 1) % slides.length;
+//   showSlide(currentSlide);
+// });
+
+// showSlide(currentSlide);
+//-------------------------------------------------
